2b73ac80917ae6342c3285db154319e80defaad9
avanish1207 <159261997+avanish1207@users.noreply.github.com>
2025-09-03 16:27:00 +0530

    test to import pokemon sprites from PokeAPI

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect,useState } from 'react'
 import { shuffle } from 'lodash';
 import './App.css';
 import Gameboard from './components/Gameboard';
@@ -6,6 +6,23 @@ import Scoreboard from './components/Scoreboard';
 
 function App() {
 
+  
+
+  const [pokeSprite, setPokeSprite]=useState('');
+  useEffect(()=>{
+    async function getPokemon(pokemon) {
+    let url=`https://pokeapi.co/api/v2/pokemon/${pokemon}`
+    try {
+      const response= await fetch(url);
+      const data=await response.json();
+      const spriteUrl=data.sprites.front_default;
+      setPokeSprite(spriteUrl);
+    }catch(error){
+      console.log(`Error: ${error}`);
+    }
+  }
+  getPokemon("ditto");
+  },[]);
   const [currentScore, setCurrentScore]=useState(0);
   const [clickedCards, setClickedCards]=useState([]);
   const [highScore, setHighScore]=useState(0);
@@ -14,7 +31,7 @@ function App() {
     id: index+1,
     name: `Card ${index+1}`
   })));
-  console.log(array);
+  // console.log(array);
 
   function handleCardClick(cardId){
     if(clickedCards.includes(cardId)){
@@ -34,6 +51,9 @@ function App() {
   return(<div>
     <Scoreboard cScore={currentScore} hScore={highScore}/>
     <Gameboard cardClickHandler={handleCardClick} cardData={array}/>
+    <div>
+      <img src={pokeSprite} alt="" />
+    </div>
   </div>);
 }
 

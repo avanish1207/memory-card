96f340c610f1f1e97de4b74a1c939097d6bc24cf
avanish1207 <159261997+avanish1207@users.noreply.github.com>
2025-09-16 18:14:57 +0530

    initialized tailwind

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,6 @@
-import './Card.css';
-
 function Card({handleClick, cardId, cardName, cardSprite}){
     return(
-        <div className='card' onClick={()=>handleClick(cardId)}>
+        <div className='card bg-black cursor-pointer' onClick={()=>handleClick(cardId)}>
             <img src={cardSprite} alt={cardName} />
         </div>
     );

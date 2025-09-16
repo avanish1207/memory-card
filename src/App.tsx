0b5c7ad7363b0f1ad4cd80bed3ef65a47829779b
avanish1207 <159261997+avanish1207@users.noreply.github.com>
2025-09-16 21:35:23 +0530

    TS changes to lodash and App

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,29 @@ import { shuffle } from 'lodash';
 import Gameboard from './components/Gameboard';
 import Scoreboard from './components/Scoreboard';
 
+interface Pokemon{
+  id: number;
+  name: string;
+  sprite: string;
+}
+
 function App() {
 
-  const [array, setArray]=useState([]);
-  const [pokeSprites, setPokeSprites]=useState([]);
-  const pokeIds=[1, 4, 7, 10, 13, 16, 19, 25, 30, 35, 40, 45];
+  const [array, setArray]=useState<Pokemon[]>([]);
+  const [pokeSprites, setPokeSprites]=useState<Pokemon[]>([]);
+  const pokeIds: number[]=[1, 4, 7, 10, 13, 16, 19, 25, 30, 35, 40, 45];
   useEffect(()=>{
     async function getPokemon() {
     try {
       const promises=pokeIds.map(pokeId=>fetch(`https://pokeapi.co/api/v2/pokemon/${pokeId}`).then(response=>response.json()));
       const results=await Promise.all(promises);
-      const sprites=results.map(data=>({
+      const sprites: Pokemon[]=results.map(data=>({
         id:data.id,
         name:data.name,
         sprite:data.sprites.front_default
       }));
       setPokeSprites(sprites);
-      setArray(sprites)
+      setArray(sprites);
       console.log(pokeSprites.length);
     }catch(error){
       console.log(`Error: ${error}`);
@@ -27,17 +33,11 @@ function App() {
   }
   getPokemon();
   },[]);
-  const [currentScore, setCurrentScore]=useState(0);
-  const [clickedCards, setClickedCards]=useState([]);
-  const [highScore, setHighScore]=useState(0);
+  const [currentScore, setCurrentScore]=useState<number>(0);
+  const [clickedCards, setClickedCards]=useState<number[]>([]);
+  const [highScore, setHighScore]=useState<number>(0);
 
-  // const [array, setArray]=useState(Array.from({length:8},(_,index)=>({ //kinda weird way of creating an array(always forget it so copy paste)
-  //   id: index+1,
-  //   name: `Card ${index+1}`
-  // })));
-  // console.log(array);
-
-  function handleCardClick(cardId){
+  function handleCardClick(cardId: number): void{
     if(clickedCards.includes(cardId)){
       setCurrentScore(0)
       setClickedCards([]);

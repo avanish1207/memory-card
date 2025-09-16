f7ff1cc0549e4388001553588e5ba7e539f9759c
avanish1207 <159261997+avanish1207@users.noreply.github.com>
2025-09-16 21:38:38 +0530

    TS changes to Card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,11 @@
-function Card({handleClick, cardId, cardName, cardSprite}){
+interface Cards{
+    cardId: number;
+    cardName: string;
+    cardSprite: string;
+    handleClick: any;
+}
+
+function Card({handleClick, cardId, cardName, cardSprite}: Cards){
     return(
         <div className='card flex justify-center bg-black cursor-pointer border-5 border-violet-500 hover:border-red-500' onClick={()=>handleClick(cardId)}>
             <img src={cardSprite} alt={cardName} className="w-30 h-30"/>
